1fad07ff37cbb7fac5646d11a4c7fc18c285aede
Carlos Madrid <dev.cmadrid@gmail.com>
2021-04-15 18:15:38 -0500

    FEAT: Finished reusable SearchBox component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -26,10 +27,11 @@ class App extends Component {
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     return (
-      <div className="app">
-        <input type="search" placeholder="Search monsters" onChange={e => 
-            this.setState({searchField: e.target.value})
-          } />
+      <div className="App">
+        <SearchBox 
+          placeholder='Search monsters'
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
